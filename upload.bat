b9ffb9425a5d1faccbdad9080c96a86bda0a27f1
Richard Licas <rlicas@esstech.com>
2025-06-11 14:52:06 -0700

    clean up

--- upload.bat
+++
@@ -1,6 +0,0 @@
-REM cd C:\Users\RLicas\Documents\LOCAL_DRIVE\PYTHON\TESTERMONITOR\TesterStatus
-git status
-git add .
-git status
-git commit -m "Update index.html" || echo "Nothing to commit"
-git push origin main
